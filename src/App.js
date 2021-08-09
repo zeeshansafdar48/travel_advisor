fd41fbd5d5b633cfd879f0d278d52d5012151b01
Zeeshan Safdar <44399893+zeeshansafdar48@users.noreply.github.com>
2021-08-09 18:42:39 +0500

    initial folder structure  and install packages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import React from 'react'
+import { CssBaseline, Grid } from '@material-ui/core'
+
+import Header from './Components/Header/Header'
+import List from './Components/List/List'
+import Map from './Components/Map/Map'
 
 export default function App() {
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <>
+     <CssBaseline />
+     <Header />
+     <Grid container style={{ width: '100%' }}>
+       <Grid item xs={12} md={4}>
+         <List />
+       </Grid>
+       <Grid item xs={12} md={8}>
+         <Map />
+       </Grid>
+     </Grid> 
+    </>
   )
 }
