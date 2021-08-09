f8574e26d79a75d7905215bf119178059ae109cc
Zeeshan Safdar <zeeshan7826@gmail.com>
2021-08-09 21:23:58 +0500

    code format

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import React from 'react'
-import { CssBaseline, Grid } from '@material-ui/core'
+import React from 'react';
+import { CssBaseline, Grid } from '@material-ui/core';
 
-import Header from './Components/Header/Header'
-import List from './Components/List/List'
-import Map from './Components/Map/Map'
+import Header from './Components/Header/Header';
+import List from './Components/List/List';
+import Map from './Components/Map/Map';
 
 export default function App() {
   return (
     <>
-     <CssBaseline />
-     <Header />
-     <Grid container style={{ width: '100%' }}>
-       <Grid item xs={12} md={4}>
-         <List />
-       </Grid>
-       <Grid item xs={12} md={8}>
-         <Map />
-       </Grid>
-     </Grid> 
+      <CssBaseline />
+      <Header />
+      <Grid container style={{ width: '100%' }}>
+        <Grid item xs={12} md={4}>
+          <List />
+        </Grid>
+        <Grid item xs={12} md={8}>
+          <Map />
+        </Grid>
+      </Grid>
     </>
-  )
+  );
 }
--- src/Components/PlaceCard/PlaceCard.js
+++ src/Components/PlaceCard/PlaceCard.js
@@ -1,9 +1,9 @@
-import React from 'react'
+import React from 'react';
 
 export default function PlaceCard() {
   return (
     <div>
       <h1>PlaceCard</h1>
     </div>
-  )
+  );
 }
