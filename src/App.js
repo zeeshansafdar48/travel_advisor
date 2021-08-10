566f5a0f70870cbad72e4eb537d58ea59911c7e8
Zeeshan Safdar <zeeshan7826@gmail.com>
2021-08-10 10:40:33 +0500

    add ratings and pin point on google map

--- src/App.js
+++ src/App.js
@@ -32,7 +32,12 @@ export default function App() {
           <List places={places} />
         </Grid>
         <Grid item xs={12} md={8}>
-          <Map setCoordinates={setCoordinates} setBounds={setBounds} coordinates={coordinates} />
+          <Map
+            setCoordinates={setCoordinates}
+            setBounds={setBounds}
+            coordinates={coordinates}
+            places={places}
+          />
         </Grid>
       </Grid>
     </>
--- src/Components/Map/Map.js
+++ src/Components/Map/Map.js
@@ -2,14 +2,14 @@ import React from 'react';
 import GoogleMapReact from 'google-map-react';
 import { Paper, Typography, useMediaQuery } from '@material-ui/core';
 import LocationOnOutlined from '@material-ui/icons/LocationOnOutlined';
-import Rating from '@material-ui/lab';
+import { Rating } from '@material-ui/lab';
 
 import useStyles from './styles';
 
-export default function Map({ setCoordinates, setBounds, coordinates }) {
+export default function Map({ setCoordinates, setBounds, coordinates, places }) {
   const classes = useStyles();
-  // isMobile will be false if device is larger than 600px
-  const isMobile = useMediaQuery('(min-width: 600px)');
+  // isDesktop will be true if device is larger than or equal to 600px
+  const isDesktop = useMediaQuery('(min-width: 600px)');
 
   return (
     <div className={classes.mapContainer}>
@@ -25,7 +25,36 @@ export default function Map({ setCoordinates, setBounds, coordinates }) {
           setBounds({ ne: e.marginBounds.ne, sw: e.marginBounds.sw });
         }}
         onChildClick={''}
-      ></GoogleMapReact>
+      >
+        {places?.map((place, index) => (
+          <div
+            key={index}
+            className={classes.markerContainer}
+            lat={Number(place?.latitude)}
+            lng={Number(place?.longitude)}
+          >
+            {!isDesktop ? (
+              <LocationOnOutlined color="secondary" fontSize="large" />
+            ) : (
+              <Paper elevation={3} className={classes.paper}>
+                <Typography className={classes.typography} gutterBottom variant="subtitle2">
+                  {place?.name}
+                </Typography>
+                <img
+                  className={classes?.pointer}
+                  src={
+                    place?.photo
+                      ? place?.photo?.images?.large?.url
+                      : 'https://www.foodserviceandhospitality.com/wp-content/uploads/2016/09/Restaurant-Placeholder-001.jpg'
+                  }
+                  alt={place?.name}
+                />
+                <Rating size="small" value={Number(place?.rating)} readOnly />
+              </Paper>
+            )}
+          </div>
+        ))}
+      </GoogleMapReact>
     </div>
   );
 }
